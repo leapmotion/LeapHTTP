76c1c081060c9cfa59bda86a242876d474476d6b
Jason Lokerson <cmercenary@gmail.com>
2016-07-14 19:46:50 -0700

    Fix missing copyright headers

--- src/leaphttp/Cookie.cpp
+++ src/leaphttp/Cookie.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
+// Copyright (C) 2012-2016 Leap Motion, Inc. All rights reserved.
 #include "stdafx.h"
 #include "Cookie.h"
 #include <algorithm>
